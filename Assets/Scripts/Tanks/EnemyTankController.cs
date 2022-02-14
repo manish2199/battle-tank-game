6be84441458fdb1efff8cfd5df6b20c814784e75
Manish <manishmistry9799@gmail.com>
2022-02-14 18:36:58 +0530

    Created Generic Singleton

---
+++ Assets/Scripts/Tanks/EnemyTankController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTankController : GenericSingleton<EnemyTankController>
+{
+	protected override void Awake()
+	{
+		base.Awake();
+	}
+}
---
+++ Assets/Scripts/Tanks/PlayerTankController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTankController : GenericSingleton<PlayerTankController>
+{
+	protected override void Awake()
+	{
+		base.Awake();
+	}
+}
