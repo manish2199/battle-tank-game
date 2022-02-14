548859ceed6e935192285698cba96741fa037deb
Manish <manishmistry9799@gmail.com>
2022-02-14 19:55:03 +0530

    Added Tank Controller

--- Assets/Scripts/Tanks/PlayerTankController.cs
+++ Assets/Scripts/Tanks/PlayerTankController.cs
@@ -4,8 +4,59 @@ using UnityEngine;
 
 public class PlayerTankController : GenericSingleton<PlayerTankController>
 {
+       
+    [SerializeField] float speed;
+     
+    [SerializeField] Joystick joystick;
+
+    [SerializeField] Rigidbody rb; 
+
+    float xAxis;
+    float zAxis;
+
+
 	protected override void Awake()
 	{
 		base.Awake();
 	}
+
+
+    void Update()
+    {
+       xAxis =joystick.Horizontal * speed;
+       zAxis = joystick.Vertical  * speed;
+
+       HandleRotation();
+    
+    }
+   
+    void HandleRotation()
+    {
+        if(zAxis > 0)
+       {
+          transform.Rotate(0 , 0 , 0);
+       }
+        if(zAxis < 0)
+       {
+          transform.Rotate(0 , -180 , 0);
+       }
+       if(xAxis > 0)
+       {
+          transform.Rotate(0 , -90 , 0);
+       }
+         if(xAxis > 0)
+       {
+          transform.Rotate(0 , 90 , 0);
+       }
+
+    }
+
+    void FixedUpdate()
+    {
+      rb.velocity = new Vector3(xAxis , 0 , zAxis);
+    }
+
+    
+
+
 }
